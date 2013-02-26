785539e20947f08a0268f01d179117d0a8359d04
Gabriel Aramburu <aramburu.gabriel@gmail.com>
2013-02-26 16:12:23 -0300

    new method

--- ruby.js
+++ ruby.js
@@ -37,3 +37,15 @@ Number.prototype.downto = function(t, cb) {
  
   return +this;
 };
+
+// Ruby = [4, 5, 6].each { |element| puts "#{element}" }
+// JS = [4, 5, 6].each(function(index) { alert(index + ": " + this); })
+Array.prototype.each = function(func) {
+  function each(array, func) {
+  for (var i=0; i<array.length; ++i) {
+    func.call(array[i], i);
+  }
+}
+ each(this, func); 
+
+};
